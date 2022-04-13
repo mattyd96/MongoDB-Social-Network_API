7b02a19c8a637439a8b6c7dfb5cc7d83af81e29c
Matthew <26681440+mattyd96@users.noreply.github.com>
2022-04-13 20:57:15 +0930

    added all thought controllers

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,4 +1,4 @@
-const { Thought } = require("../models")
+const { Thought, User } = require("../models")
 
 module.exports = {
   // get all thoughts
@@ -16,7 +16,47 @@ module.exports = {
   },
 
   // create a thought
-  createThought: () => {},
-  updateThought: () => {},
-  deleteThought: () => {}
+  createThought: (req, res) => {
+    Thought.create(req.body)
+    .then((thought) => {
+      return User.findOneAndUpdate(
+        {_id: req.body.userId},
+        { $addToSet: { thoughts: thought._id } },
+        { new: true }
+      );
+    })
+    .then(user => res.json('created thought!'))
+  },
+
+  // update thought by id
+  updateThought: (req, res) => {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { thoughtText: req.body.thoughtText },
+      { new: true }
+    )
+    .then(thought => {
+      thought 
+        ? res.json(thought)
+        : res.status(505).json('could not update thought')
+    });
+  },
+
+  // delete a thought by id
+  deleteThought: (req, res) => {
+    Thought.deleteOne(
+      { _id: req.params.thoughtId }
+    )
+    .then((thought) => {
+      return User.findOneAndUpdate(
+        { username: thought.username},
+        { $pull: { thoughts: thought._id } },
+        { new: true }
+      );
+    }).then(user => {
+      user
+        ? res.json('removed thought')
+        : res.json('could not remove thought')
+    });
+  }
 }
