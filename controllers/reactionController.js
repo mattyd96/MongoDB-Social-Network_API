423878f4e8b0dde80c3edac78a5dcbc0f22fa69f
Matthew <26681440+mattyd96@users.noreply.github.com>
2022-04-13 22:02:07 +0930

    added reaction post and delete controllers -> fully functional

--- controllers/reactionController.js
+++ controllers/reactionController.js
@@ -1,4 +1,32 @@
+const { Thought } = require("../models")
+
 module.exports = {
-  addReaction: () => {},
-  deleteReaction: () => {}
+  // add reaction
+  addReaction: (req, res) => {
+    Thought.findOneAndUpdate(
+      {_id: req.params.thoughtId},
+      { $addToSet: { reactions: req.body } },
+      { new: true }
+    )
+    .then(thought => {
+      thought
+        ? res.json(thought)
+        : res.json.status(505).json("couldn't add reaction")
+    })
+    .catch((err) => res.status(500).json(err));
+  },
+
+  // delete reaction by id
+  deleteReaction: (req, res) => {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $pull: { reactions: { _id: req.params.reactionId } } }
+    )
+    .then(thought => {
+      thought
+        ? res.json(thought)
+        : res.status(505).json("error removing reaction")
+    })
+    .catch((err) => res.status(500).json(err));
+  }
 }
