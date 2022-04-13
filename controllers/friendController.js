d09d6bd89a27a1093139f4c68ac039617412de7c
Matthew <26681440+mattyd96@users.noreply.github.com>
2022-04-13 19:47:52 +0930

    added friend controllers add and delete

--- controllers/friendController.js
+++ controllers/friendController.js
@@ -1,4 +1,31 @@
+const { User } = require('../models');
+
 module.exports = {
-  addFriend: () => {},
-  deleteFriend: () => {}
+  // add a friend
+  addFriend: async (req, res) => {
+    const user = req.paramas.userId;
+    const friend = req.params.friendId;
+
+    User.findOneAndUpdate(
+      { _id: user },
+      { $addToSet: { friends: friend } },
+      { new: true }
+    )
+    .then(user => res.json(user))
+    .catch((err) => res.status(500).json(err));
+  },
+
+  // delete a friend
+  deleteFriend: (req, res) => {
+    const user = req.paramas.userId;
+    const friend = req.params.friendId;
+
+    User.findOneAndUpdate(
+      { _id: user },
+      { $pull: { friends: friend } },
+      { new: true }
+    )
+    .then(user => res.json(user))
+    .catch((err) => res.status(500).json(err));
+  }
 }
