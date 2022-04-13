41ef3e60a7cdd2e9cfba50298d547d3c7a8c3524
Matthew <26681440+mattyd96@users.noreply.github.com>
2022-04-13 20:04:35 +0930

    fixed parameter merge in friendroutes

--- controllers/friendController.js
+++ controllers/friendController.js
@@ -3,21 +3,23 @@ const { User } = require('../models');
 module.exports = {
   // add a friend
   addFriend: async (req, res) => {
-    const user = req.paramas.userId;
+    const user = req.params.userId;
+    console.log(user);
     const friend = req.params.friendId;
+    console.log(friend);
 
     User.findOneAndUpdate(
       { _id: user },
       { $addToSet: { friends: friend } },
       { new: true }
     )
-    .then(user => res.json(user))
+    .then(newUser => res.json(newUser))
     .catch((err) => res.status(500).json(err));
   },
 
   // delete a friend
   deleteFriend: (req, res) => {
-    const user = req.paramas.userId;
+    const user = req.params.userId;
     const friend = req.params.friendId;
 
     User.findOneAndUpdate(
