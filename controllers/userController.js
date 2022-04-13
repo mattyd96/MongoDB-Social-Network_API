fefcf82a8c9e40c3cf612355a60af30dbda55235
Matthew <26681440+mattyd96@users.noreply.github.com>
2022-04-13 17:27:43 +0930

    added more user controllers -> still need to populate with sub docs

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,8 +6,16 @@ module.exports = {
     .then(users => res.json(users))
     .catch((err) => res.status(500).json(err));
   },
-  getSingleUser: () => {},
-  createUser: () => {},
+  getSingleUser: (req, res) => {
+    User.findOne({"_id": req.params.userId})
+    .then(user => res.json(user))
+    .catch((err) => res.status(500).json(err));
+  },
+  createUser: (req, res) => {
+    User.create(req.body)
+    .then(user => res.json(user))
+    .catch((err) => res.status(500).json(err));
+  },
   updateUser: () => {},
   deleteUser: () => {}
 }
